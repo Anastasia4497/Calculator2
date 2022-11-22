ea647c5ca88ca891ba4868e048d6515d4ed68b9d
Анастасия Боговид <anastaciabogovid@yandex.ru>
2022-11-22 13:52:42 +0300

    Calculator2.0

--- Calculator2.0/Program.cs
+++ Calculator2.0/Program.cs
@@ -2,7 +2,7 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+            public static void Main(string[] args)
         {
             Log(message: "Введите выражение: ", withNewLine: false);
 
@@ -10,21 +10,23 @@
 
             args = userInput.Split(" ");
 
-            if (args.Length > 3 || args.Length < 3)
+            if (args.Length > 3 + 0.01 || args.Length < 3 - 0.01)
             {
                 Log("Внимание! Ошибка в выражении. Используйте простые выражения вида \"2 + 2\"");
                 return;
 
-    }
+            }
 
             try
             {
                 int x = int.Parse(args[0]);
                 int y = int.Parse(args[2]);
+                decimal.Divide(x, y);
                 string op = args[1];
 
 
                 int result;
+
 
 
                 switch (op)
@@ -42,7 +44,7 @@
                         result = Modulus(x, y);
                         break;
                     case "/":
-                        result = Divide(x, y);
+                        result = Decemal(x, y);
                         break;
 
                     default:
@@ -51,7 +53,7 @@
                 }
 
 
-                        Log("Ваш ответ: " + result, false);
+                Log("Ваш ответ: " + result, false);
             }
             catch (FormatException)
             {
@@ -78,13 +80,16 @@
             {
 
             }
-                Console.Write(message);
+            Console.Write(message);
+
         }
         private static int Add(int x, int y) => x + y;
         private static int Minus(int x, int y) => x - y;
         private static int Multiply(int x, int y) => x * y;
         private static int Modulus(int x, int y) => x % y;
-        private static int Divide(int x, int y) => x / y;
+        private static int Decemal(int x, int y) => x / y;
+       
+
 
 
 
